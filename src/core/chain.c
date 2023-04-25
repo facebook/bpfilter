66cc3e1dae278c6c38a04969fc496db5bd229844
Quentin Deslandes <qde@naccy.de>
2023-04-25 17:12:32 +0200

    daemon: add chain structure and convenience functions
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ src/core/chain.c
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ */
+
+#include "chain.h"
+
+#include <errno.h>
+#include <stdlib.h>
+
+int bf_chain_new(struct bf_chain **chain)
+{
+	struct bf_chain *_chain;
+
+	_chain = calloc(1, sizeof(*_chain));
+	if (!_chain)
+		return -ENOMEM;
+
+	bf_list_init(&_chain->rules);
+
+	*chain = _chain;
+
+	return 0;
+}
+
+void bf_chain_free(struct bf_chain **chain)
+{
+	if (!*chain)
+		return;
+
+	bf_list_foreach(&(*chain)->rules, node) {
+		struct bf_rule *rule = bf_list_node_data(node);
+		bf_rule_free(&rule);
+	}
+
+	bf_list_clean(&(*chain)->rules);
+
+	free(*chain);
+	*chain = NULL;
+}
