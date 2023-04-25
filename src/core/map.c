899276050bd14005df1d6909eff6b2815bb82ab3
Quentin Deslandes <qde@naccy.de>
2023-04-25 18:19:40 +0200

    daemon: add map structure and unit tests
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ src/core/map.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ */
+
+#include "map.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int bf_map_new(bf_map **map, size_t nelem)
+{
+	bf_map *_map;
+
+	assert(map);
+
+	_map = calloc(1, sizeof(*_map));
+	if (!_map)
+		return -ENOMEM;
+
+	if (!hcreate_r(nelem, _map)) {
+		free(_map);
+		return -errno;
+	}
+
+	*map = _map;
+
+	return 0;
+}
+
+void bf_map_free(bf_map **map)
+{
+	hdestroy_r(*map);
+	free(*map);
+	*map = NULL;
+}
+
+int bf_map_find(bf_map *map, const char *key, void **value)
+{
+	const ENTRY needle = { .key = (char *)key };
+	ENTRY *found;
+
+	assert(map);
+	assert(key);
+	assert(value);
+
+	if (!hsearch_r(needle, FIND, &found, map))
+		return -ENOENT;
+
+	*value = found->data;
+
+	return 0;
+}
+
+int bf_map_upsert(bf_map *map, const char *key, void *value)
+{
+	const ENTRY needle = { .key = (char *)key, .data = value };
+	ENTRY *found;
+
+	assert(map);
+	assert(key);
+	assert(value);
+
+	if (!hsearch_r(needle, ENTER, &found, map))
+		return -errno;
+
+	found->key = (char *)key;
+	found->data = value;
+
+	return 0;
+}
---
+++ src/core/map.h
@@ -0,0 +1,61 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ */
+
+#pragma once
+
+#define _GNU_SOURCE
+
+#include <search.h>
+#include <stddef.h>
+
+typedef struct hsearch_data bf_map;
+
+/**
+ * @brief Create a new map.
+ *
+ * Allocate and initialize a new map object.
+ *
+ * @param map Return argument to store the map's pointer into. If an error
+ *  occur during map creating, this argument remain unchanged.
+ * 	Must be non-NULL.
+ * @param nelem Maximum number of element expected to be stored in the map.
+ * @return 0 on success, or negative error code on failure.
+ */
+int bf_map_new(bf_map **map, size_t nelem);
+
+/**
+ * @brief Deinitialize and free a map.
+ *
+ * Data contained in the map is not modified or freed. Only the memory allocated
+ * to maintain the map itself is freed. Once this function returns, the map
+ * should not be used.
+ *
+ * @param map Map object to cleanup. Must be non-NULL.
+ */
+void bf_map_free(bf_map **map);
+
+/**
+ * @brief Look for a key in the map.
+ *
+ * @param map Map to search into. Must be non-NULL.
+ * @param key Key to look for. Must be non-NULL.
+ * @param value Return argument, used to store the address of the value, if
+ *  found. This argument is not modified if the search fails. Must be non-NULL.
+ * @return 0 on success, or negative error code on failure.
+ */
+int bf_map_find(bf_map *map, const char *key, void **value);
+
+/**
+ * @brief Insert a new key/value pair in the map, update the entry if the
+ * 	key already exists.
+ *
+ * @param map Map to insert into. Must be non-NULL.
+ * @param key Key to insert a value for. Must be non-NULL.
+ * @param value Value to insert into the map. If the key already exists, its
+ * 	value is replaced. The old value is discarded but not freed. Must be
+ * 	non-NULL.
+ * @return 0 on success, or negative error code on failure.
+ */
+int bf_map_upsert(bf_map *map, const char *key, void *value);
