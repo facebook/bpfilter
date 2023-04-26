6ef97957d82b767165036c8b04dd65c97c1577c8
Quentin Deslandes <qde@naccy.de>
2023-04-26 12:25:08 +0200

    daemon: add codegen and context boilerplate
    Add struct bf_context containing a list of codegen for each supported
    hook.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ src/core/context.c
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 Meta Platforms, Inc. and affiliates.
+ */
+
+#include "context.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "core/list.h"
+#include "generator/codegen.h"
+
+void bf_context_init(struct bf_context *context)
+{
+    assert(context);
+
+    for (int i = 0; i < __BF_HOOK_MAX; ++i)
+        bf_list_init(&context->hooks[i],
+                     (bf_list_ops[]) {{.free = bf_codegen_free}});
+}
+
+void bf_context_clean(struct bf_context *context)
+{
+    for (int i = 0; i < __BF_HOOK_MAX; ++i)
+        bf_list_clean(&context->hooks[i]);
+}
--- src/generator/codegen.h
+++ src/generator/codegen.h
@@ -5,7 +5,24 @@
 
 #pragma once
 
+#define __cleanup_bf_codegen__ __attribute__((cleanup(bf_codegen_free)))
+
 struct bf_codegen
 {
     int placeholder;
 };
+
+/**
+ * @brief Allocate and initialise a new codegen.
+ *
+ * @param codegen Codegen to initialise. Can't be NULL.
+ * @return int 0 on success, negative error code on failure.
+ */
+int bf_codegen_new(struct bf_codegen **codegen);
+
+/**
+ * @brief Free a codegen.
+ *
+ * @param codegen Codegen to free. Can't be NULL.
+ */
+void bf_codegen_free(struct bf_codegen **codegen);
