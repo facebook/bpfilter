11d15a126a4b825525901c5c8706e0c7b2655bd4
Quentin Deslandes <qde@naccy.de>
2023-04-26 16:05:59 +0200

    shared: allow for bf_response to be created from a raw buffer
    Add bf_response to be created from a buffer and a size. This allow for
    easy copy of bf_response objects.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

--- shared/include/bpfilter/shared/response.h
+++ shared/include/bpfilter/shared/response.h
@@ -36,6 +36,19 @@ struct bf_response
     };
 };
 
+/**
+ * @brief Allocate a response from raw data.
+ *
+ * @param response Response to allocate. Can't be NULL.
+ * @param data Response's data. Will be copied inside the response. Can't be
+ * NULL.
+ * @param data_len Full length of the response's data, including fixed and
+ *  variable length fields.
+ * @return 0 on success, negative errno value on error.
+ */
+int bf_response_new_raw(struct bf_response **response, const char *data,
+                        size_t data_len);
+
 int bf_response_new_success(struct bf_response **response, size_t data_len,
                             const char *data);
 int bf_response_new_failure(struct bf_response **response, int error);
