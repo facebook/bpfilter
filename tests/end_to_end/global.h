f6f0d0c6532ace2cdcfea070e2bd2294e7657ca2
Quentin Deslandes <qde@naccy.de>
2023-04-28 14:57:53 +0200

    tests: add ipt.c to test bpfilter's iptables interface
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ tests/end_to_end/global.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 Meta Platforms, Inc. and affiliates.
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define _log(fd, fmt, ...) ({ fprintf(fd, fmt "\n", ##__VA_ARGS__); })
+#define ok(fmt, ...) _log(stdout, "ok : " fmt, ##__VA_ARGS__)
+#define err(fmt, ...) _log(stderr, "err: " fmt, ##__VA_ARGS__)
