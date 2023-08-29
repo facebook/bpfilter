6001c91da06015cb1208d32b8788bad4fa413a1c
Quentin Deslandes <qde@naccy.de>
2023-08-29 11:20:18 -0400

    generator: add BPF bytecode stub generation functions
    Add stub.{h,c} containing function to generate BPF bytecode stubs, such
    as:
    - Get a dynptr from an sk_buff.
    - Get layers 2, 3, and 4 slices from an sk_buff.
    
    Those function ease generation of repeatitive BPF bytecode and work with
    full knowledge of bpfilter runtime, hence can access the runtime
    context.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ src/generator/stub.c
@@ -0,0 +1,199 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 Meta Platforms, Inc. and affiliates.
+ */
+
+#include "generator/stub.h"
+
+#include <linux/bpf.h>
+
+#include <arpa/inet.h>
+#include <assert.h>
+
+#include "external/filter.h"
+#include "generator/program.h"
+
+int bf_stub_memclear(struct bf_program *program, enum bf_reg addr_reg,
+                     size_t size)
+{
+    assert(program);
+    assert(!(size % 8));
+
+    for (size_t i = 0; i < size; i += 8)
+        EMIT(program, BPF_ST_MEM(BPF_DW, addr_reg, i, 0));
+
+    return 0;
+}
+
+int bf_stub_make_ctx_skb_dynptr(struct bf_program *program, enum bf_reg skb_reg)
+{
+    assert(program);
+
+    // BF_ARG_1: address of the skb.
+    if (BF_ARG_1 != skb_reg)
+        EMIT(program, BPF_MOV64_IMM(BF_ARG_1, skb_reg));
+
+    // BF_ARG_2: flags.
+    EMIT(program, BPF_MOV64_IMM(BF_ARG_2, 0));
+
+    // BF_ARG_1: address of the dynptr in the context.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_3, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_3, BF_PROG_CTX_OFF(dynptr)));
+
+    EMIT_KFUNC_CALL(program, "bpf_dynptr_from_skb");
+
+    // If an error occurs, quit the program.
+    EMIT_FIXUP(program, BF_CODEGEN_FIXUP_END_OF_CHAIN,
+               BPF_JMP_IMM(BPF_JNE, BF_REG_RET, 0, 0));
+
+    return 0;
+}
+
+int bf_stub_get_l2_eth_hdr(struct bf_program *program)
+{
+    assert(program);
+
+    // BF_ARG_1: address of the dynptr in the context.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_1, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_1, BF_PROG_CTX_OFF(dynptr)));
+
+    // BF_ARG_2: offset
+    EMIT(program, BPF_MOV64_IMM(BF_ARG_2, 0));
+
+    // BF_ARG_3: pointer to the buffer to store L2 header.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_3, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_3, BF_PROG_CTX_OFF(l2raw)));
+
+    // BF_ARG_4: size of the L2 header buffer.
+    EMIT(program, BPF_MOV64_IMM(BF_ARG_4, sizeof(struct ethhdr)));
+
+    EMIT_KFUNC_CALL(program, "bpf_dynptr_slice");
+
+    // If an error occurs, quit the program.
+    EMIT_FIXUP(program, BF_CODEGEN_FIXUP_END_OF_CHAIN,
+               BPF_JMP_IMM(BPF_JEQ, BF_REG_RET, 0, 0));
+
+    // Copy the L2 header pointer to BF_REG_L2.
+    EMIT(program, BPF_MOV64_REG(BF_REG_L2, BF_REG_RET));
+
+    // Load L2 ethertype
+    EMIT(program, BPF_LDX_MEM(BPF_H, BF_REG_1, BF_REG_L2,
+                              offsetof(struct ethhdr, h_proto)));
+
+    // Quit the program if L3 is not IPv4.
+    EMIT_FIXUP(program, BF_CODEGEN_FIXUP_END_OF_CHAIN,
+               BPF_JMP_IMM(BPF_JNE, BF_REG_1, ntohs(ETH_P_IP), 0));
+
+    // Update L3 header offset.
+    EMIT(program, BPF_ST_MEM(BPF_W, BF_REG_CTX, BF_PROG_CTX_OFF(l3_offset),
+                             sizeof(struct ethhdr)));
+
+    return 0;
+}
+
+int bf_stub_get_l3_ipv4_hdr(struct bf_program *program)
+{
+    assert(program);
+
+    // BF_ARG_1: address of the dynptr in the context.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_1, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_1, BF_PROG_CTX_OFF(dynptr)));
+
+    // BF_ARG_2: L3 header offset from the context.
+    EMIT(program,
+         BPF_LDX_MEM(BPF_W, BF_ARG_2, BF_REG_CTX, BF_PROG_CTX_OFF(l3_offset)));
+
+    // BF_ARG_3: pointer to the buffer.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_3, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_3, BF_PROG_CTX_OFF(l3raw)));
+
+    // BF_ARG_4: size of the buffer
+    EMIT(program, BPF_MOV64_IMM(BF_ARG_4, sizeof(struct iphdr)));
+
+    EMIT_KFUNC_CALL(program, "bpf_dynptr_slice");
+
+    // If an error occurs, quit the program.
+    EMIT_FIXUP(program, BF_CODEGEN_FIXUP_END_OF_CHAIN,
+               BPF_JMP_IMM(BPF_JEQ, BF_REG_RET, 0, 0));
+
+    // Copy the L3 header pointer to BF_REG_L3.
+    EMIT(program, BPF_MOV64_REG(BF_REG_L3, BF_REG_RET));
+
+    // Load ip.ihl into BF_REG_1
+    EMIT(program, BPF_LDX_MEM(BPF_B, BF_REG_1, BF_REG_L3, 0));
+
+    // Only keep the 4 IHL bits
+    EMIT(program, BPF_ALU64_IMM(BPF_RSH, BF_REG_1, 4));
+
+    // Convert the number of words stored in ip.ihl into a number of bytes.
+    EMIT(program, BPF_ALU64_IMM(BPF_LSH, BF_REG_1, 2));
+
+    // Store the L3 offst in BF_REG_2
+    EMIT(program,
+         BPF_LDX_MEM(BPF_W, BF_REG_2, BF_REG_CTX, BF_PROG_CTX_OFF(l3_offset)));
+
+    // Add the L3 offset to the L4 offset
+    EMIT(program, BPF_ALU64_REG(BPF_ADD, BF_REG_1, BF_REG_2));
+
+    // Store the L4 header offset back into the context.
+    EMIT(program,
+         BPF_STX_MEM(BPF_W, BF_REG_CTX, BF_REG_1, BF_PROG_CTX_OFF(l4_offset)));
+
+    // Copy the L4 protocol into BF_REG_1
+    EMIT(program, BPF_LDX_MEM(BPF_B, BF_REG_1, BF_REG_L3,
+                              offsetof(struct iphdr, protocol)));
+
+    // Store the L4 protocol into the context.
+    EMIT(program,
+         BPF_STX_MEM(BPF_W, BF_REG_CTX, BF_REG_1, BF_PROG_CTX_OFF(l4_proto)));
+
+    return 0;
+}
+
+int bf_stub_get_l4_hdr(struct bf_program *program)
+{
+    assert(program);
+
+    // BF_ARG_1: address of the dynptr in the context.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_1, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_1, BF_PROG_CTX_OFF(dynptr)));
+
+    // BF_ARG_2: L4 header offset from the context.
+    EMIT(program,
+         BPF_LDX_MEM(BPF_W, BF_ARG_2, BF_REG_CTX, BF_PROG_CTX_OFF(l4_offset)));
+
+    // BF_ARG_3: pointer to the buffer.
+    EMIT(program, BPF_MOV64_REG(BF_ARG_3, BF_REG_CTX));
+    EMIT(program, BPF_ALU64_IMM(BPF_ADD, BF_ARG_3, BF_PROG_CTX_OFF(l4raw)));
+
+    // Load L4 protocol from the context.
+    EMIT(program,
+         BPF_LDX_MEM(BPF_W, BF_REG_4, BF_REG_CTX, BF_PROG_CTX_OFF(l4_proto)));
+
+    // If L4 protocol is TCP.
+    EMIT(program, BPF_JMP_IMM(BPF_JEQ, BF_REG_4, IPPROTO_TCP, 2));
+
+    // If L4 protocol is UDP.
+    EMIT(program, BPF_JMP_IMM(BPF_JEQ, BF_REG_4, IPPROTO_UDP, 3));
+
+    // Protocol is not supported, quit the program.
+    EMIT_FIXUP(program, BF_CODEGEN_FIXUP_END_OF_CHAIN, BPF_JMP_A(0));
+
+    // If TCP
+    EMIT(program, BPF_MOV64_IMM(BF_ARG_4, sizeof(struct tcphdr)));
+    EMIT(program, BPF_JMP_A(1));
+
+    // If UDP
+    EMIT(program, BPF_MOV64_IMM(BF_ARG_4, sizeof(struct udphdr)));
+
+    EMIT_KFUNC_CALL(program, "bpf_dynptr_slice");
+
+    // If an error occurs, quit the program.
+    EMIT_FIXUP(program, BF_CODEGEN_FIXUP_END_OF_CHAIN,
+               BPF_JMP_IMM(BPF_JEQ, BF_REG_RET, 0, 0));
+
+    // Copy the L3 header pointer to BF_REG_L3.
+    EMIT(program, BPF_MOV64_REG(BF_REG_L4, BF_REG_RET));
+
+    return 0;
+}
---
+++ src/generator/stub.h
@@ -0,0 +1,118 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 Meta Platforms, Inc. and affiliates.
+ */
+
+#pragma once
+
+#include <stddef.h>
+
+#include "generator/reg.h"
+
+struct bf_program;
+
+/**
+ * @brief Emit instructions to clear a memory region.
+ *
+ * Generate BPF instructions to clear (set to 0) a memory region, from a
+ * register containing the address of the memory region to clear, and the size.
+ *
+ * @warning The memory area *must* be aligned on 8 bytes (address and size), as
+ * the region is cleared 8 bytes at a time.
+ *
+ * @param program Program to emit instructions into.
+ * @param addr_reg Register containing the address to clear.
+ * @param size Size of the memory region to clear.
+ * @return 0 on success, or negative errno value on error.
+ */
+int bf_stub_memclear(struct bf_program *program, enum bf_reg addr_reg,
+                     size_t size);
+
+/**
+ * @brief Emit instructions to initialise the context's dynptr on the packet.
+ *
+ * Store bpf_dynptr_from_skb argument into:
+ * - BF_ARG_1: pointer to the @p skb. This is the address provided by @p
+ *   skb_reg. If @p skb_reg is already BF_ARG_1, skip this instruction.
+ * - BF_ARG_2: flags, 0.
+ * - BF_ARG_3: address to the dynptr structure located in the context.
+ * Then, call bpf_dynptr_from_skb(). If the return value is different from 0,
+ * jump to the end of the program.
+ *
+ * @param program Program to emit instructions into.
+ * @param skb_reg Scratch register containing the pointer to the skb.
+ * @return 0 on success, or negative errno value on error.
+ */
+int bf_stub_make_ctx_skb_dynptr(struct bf_program *program,
+                                enum bf_reg skb_reg);
+
+/**
+ * @brief Emit instructions to get a dynptr slice for the packet's L2 Ethernet
+ * header.
+ *
+ * Store bpf_dynptr_slice arguments into:
+ * - BF_ARG_1: pointer to the dynptr located in the context.
+ * - BF_ARG_2: offset of the slice, in the dynptr. Always 0, as L2 Ethernet is
+ *   the first header.
+ * - BF_ARG_3: pointer to the buffer to store the slice into. Each header buffer
+ *   is located in the context.
+ * - BF_ARG_4: size of the buffer. Always ETH_HLEN, as L2 Ethernet is the first
+ *   header.
+ * Then, call bpf_dynptr_slice(). If the return value is different from 0, jump
+ * to the end of the program.
+ * Finally:
+ * - Copy the address of the header into BF_REG_L2.
+ * - Load the header's h_proto field to determine the L3 protocol. If L3
+ *   protocol is not IPv4 (the only supported protocol for now), jump to the
+ *   end of the program.
+ * - Store the offset of the L3 header in the context.
+ *
+ * @param program Program to emit instructions into.
+ * @return 0 on success, or negative errno value on error.
+ */
+int bf_stub_get_l2_eth_hdr(struct bf_program *program);
+
+/**
+ * @brief Emit instructions to get a dynptr slice for the packet's L3 IPv4
+ * header.
+ *
+ * Store bpf_dynptr_slice arguments into:
+ * - BF_ARG_1: pointer to the dynptr located in the context.
+ * - BF_ARG_2: offset of the slice, in the dynptr. Get it from l3_offset field
+ *   in the context. This field is either 0, or set when processing layer 2
+ *   header.
+ * - BF_ARG_3: pointer to the buffer to store the slice into.
+ * - BF_ARG_4: size of the buffer, expected to be an IPv4 header.
+ * Then, call bpf_dynptr_slice(). If the return value is different from 0, jump
+ * to the end of the program.
+ * Finally:
+ * - Store the address of the header into BF_REG_L3.
+ * - Compute the offset of the L4 header:
+ *   - Load ip.ihl into BF_REG_1
+ *   - Add ctx.l3_offset to it.
+ *   - Copy it back to the context.
+ * - Store L4 protocol into the context.
+ *
+ * @param program Program to emit instructions into.
+ * @return 0 on success, or negative errno value on error.
+ */
+int bf_stub_get_l3_ipv4_hdr(struct bf_program *program);
+
+/**
+ * @brief Emit instructions to get a dynptr slice for the packet's L4 header.
+ *
+ * Store bpf_dynptr_slice arguments into:
+ * - BF_ARG_1: pointer to the dynptr located in the context.
+ * - BF_ARG_2: offset of the slice, in the dynptr. Get it from l4_offset field
+ *   in the context. This field is set when processing layer 2 header.
+ * - BF_ARG_3: pointer to the buffer to store the slice into.
+ * - BF_ARG_4: size of the buffer, need to be computed depending ctx.l4_proto.
+ *   If ctx.l4_proto is not supported, jump to the end of the program.
+ * Then, call bpf_dynptr_slice(). If the return value is different from 0, jump
+ * to the end of the program.
+ * Finally, copy the address of the header into BF_REG_L4.
+ *
+ * @param program Program to emit instructions into.
+ * @return 0 on success, or negative errno value on error.
+ */
+int bf_stub_get_l4_hdr(struct bf_program *program);
