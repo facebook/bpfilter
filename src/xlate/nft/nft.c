a214bc7d7d0cfe13bafb615a913f7d488bfe57a8
Quentin Deslandes <qde@naccy.de>
2023-11-14 15:25:53 -0500

    xlate: nft: dump Netlink messages coming from nftables
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

--- src/xlate/nft/nft.c
+++ src/xlate/nft/nft.c
@@ -3,11 +3,17 @@
  * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
  */
 
+#include <linux/netfilter/nfnetlink.h>
+
+#include <stdlib.h>
+
 #include "core/logger.h"
 #include "core/marsh.h"
 #include "shared/request.h"
 #include "shared/response.h"
 #include "xlate/front.h"
+#include "xlate/nft/nlmsg.h"
+#include "xlate/nft/nlpart.h"
 
 static int _bf_nft_setup(void);
 static int _bf_nft_teardown(void);
@@ -34,15 +40,6 @@ static int _bf_nft_teardown(void)
     return 0;
 }
 
-static int _bf_nft_request_handler(struct bf_request *request,
-                                   struct bf_response **response)
-{
-    UNUSED(request);
-    UNUSED(response);
-
-    return 0;
-}
-
 static int _bf_nft_marsh(struct bf_marsh **marsh)
 {
     UNUSED(marsh);
@@ -55,4 +52,24 @@ static int _bf_nft_unmarsh(struct bf_marsh *marsh)
     UNUSED(marsh);
 
     return 0;
+}
+
+static int _bf_nft_request_handler(struct bf_request *request,
+                                   struct bf_response **response)
+{
+    bf_assert(request);
+    bf_assert(response);
+
+    _cleanup_bf_nlmsg_ struct bf_nlmsg *req = NULL;
+    _cleanup_bf_nlmsg_ struct bf_nlmsg *res = NULL;
+    int r;
+
+    r = bf_nlmsg_new_from_stream(&req, (struct nlmsghdr *)request->data,
+                                 request->data_len);
+    if (r < 0)
+        return bf_err_code(r, "failed to create bf_nlmsg from request");
+
+    bf_nlmsg_dump(req, sizeof(struct nfgenmsg), NULL);
+
+    return -ENOTSUP;
 }
---
+++ src/xlate/nft/nft.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ */
+
+#pragma once
+
+#define BF_NF_NL_MSGTYPE_MASK 0xff
