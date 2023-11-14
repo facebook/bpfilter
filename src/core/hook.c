c53157f39eeb3e27c73cb85829979d38b0669101
Quentin Deslandes <qde@naccy.de>
2023-11-14 15:34:08 -0500

    core: add support for nftables INGRESS hook
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

--- src/core/hook.c
+++ src/core/hook.c
@@ -30,6 +30,7 @@ const char *bf_hook_to_str(enum bf_hook hook)
 unsigned int bf_hook_to_bpf_prog_type(enum bf_hook hook)
 {
     static const unsigned int prog_type[] = {
+        [BF_HOOK_NFT_INGRESS] = BPF_PROG_TYPE_XDP,
         [BF_HOOK_TC_INGRESS] = BPF_PROG_TYPE_SCHED_CLS,
         [BF_HOOK_IPT_PRE_ROUTING] = BPF_PROG_TYPE_NETFILTER,
         [BF_HOOK_IPT_LOCAL_IN] = BPF_PROG_TYPE_NETFILTER,
@@ -49,6 +50,7 @@ unsigned int bf_hook_to_bpf_prog_type(enum bf_hook hook)
 enum bf_flavor bf_hook_to_flavor(enum bf_hook hook)
 {
     static const enum bf_flavor flavors[] = {
+        [BF_HOOK_NFT_INGRESS] = BF_FLAVOR_XDP,
         [BF_HOOK_TC_INGRESS] = BF_FLAVOR_TC,
         [BF_HOOK_IPT_PRE_ROUTING] = BF_FLAVOR_NF,
         [BF_HOOK_IPT_LOCAL_IN] = BF_FLAVOR_NF,
@@ -68,6 +70,7 @@ enum bf_flavor bf_hook_to_flavor(enum bf_hook hook)
 enum bpf_attach_type bf_hook_to_attach_type(enum bf_hook hook)
 {
     static const enum bpf_attach_type hooks[] = {
+        [BF_HOOK_NFT_INGRESS] = 0,
         [BF_HOOK_TC_INGRESS] = 0,
         [BF_HOOK_IPT_PRE_ROUTING] = 0,
         [BF_HOOK_IPT_LOCAL_IN] = BPF_NETFILTER,
