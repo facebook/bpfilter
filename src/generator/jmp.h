4598911c35b3e965069bcd10f14335f5ad18a865
Quentin Deslandes <qde@naccy.de>
2024-02-14 23:12:40 +0100

    generator: add support for local BPF jump contexts
    bpfilter emits jump instructions to the BPF programs. These jumps are
    relative to the current instruction pointer. This means that the jump
    offset need to be carefully updated if the program is generated
    differently:
    
    EMIT(program, BPF_JMP_IMM(BPF_JEQ, BF_REG_2, 0, 2));
    EMIT(program,
        BPF_MOV64_IMM(BF_REG_RET, program->runtime.ops->get_verdict(
            BF_VERDICT_ACCEPT)));
    EMIT(program, BPF_EXIT_INSN())
    
    The jump instruction is used to jump after the BPF_EXIT_INSN() call if
    if condition is true. However, if any instruction is added before
    BPF_EXIT_INSN(), the jump offset needs to be updated. This type of
    code prone to error is common in bpfilter for error management.
    
    Local BPF jump contexts are introduced to handle this issue. A context
    define a scope-bound set of instructions to be executed if the condition
    is false, with the context automatically updating the jump offsets:
    
    {
        _cleanup_bf_jmpctx_ struct bf_jmpctx ctx =
            bf_jmpctx_get(program, BPF_JMP_IMM(BPF_JEQ, BF_REG_2, 0, 0));
    
        EMIT(program,
            BPF_MOV64_IMM(BF_REG_RET, program->runtime.ops->get_verdict(
                BF_VERDICT_ACCEPT)));
        EMIT(program, BPF_EXIT_INSN());
    }
    
    A context is defined first thing in the scope, with a jump instruction.
    Some instructions are added to the program from the scope. When the
    scope closes, the context is destroyed and the jump offsets is updated.
    Calling EMIT_* from the scope won't require any update to the jump
    offsets, as it will automatically be handled by the context.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ src/generator/jmp.h
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 Meta Platforms, Inc. and affiliates.
+ */
+
+#pragma once
+
+#include <linux/bpf.h>
+
+#include <stddef.h>
+
+/**
+ * @file jmp.h
+ *
+ * @ref bf_jmpctx is a helper structure to manage jump instructions in the
+ * program. It is used to emit a jump instruction and automatically clean it up
+ * when the scope is exited, thanks to GCC's @c cleanup attribute.
+ *
+ * Example:
+ * @code{.c}
+ *  // Within a function body
+ *  {
+ *      _cleanup_bf_jmpctx_ struct bf_jmpctx ctx =
+ *          bf_jmpctx_get(program, BPF_JMP_IMM(BPF_JEQ, BF_REG_2, 0, 0));
+ *
+ *      EMIT(program,
+ *          BPF_MOV64_IMM(BF_REG_RET, program->runtime.ops->get_verdict(
+ *              BF_VERDICT_ACCEPT)));
+ *      EMIT(program, BPF_EXIT_INSN());
+ *  }
+ * @endcode
+ * \c ctx is a variable local to the scope, marked with \c _cleanup_bf_jmpctx_ .
+ * The second argument to \c bf_jmpctx_get is the jump instruction to emit, with
+ * the correct condition. When the scope is exited, the jump instruction is
+ * automatically updated to point to the current instruction, which is after the
+ * scope.
+ *
+ * Hence, all the instructions emitted within the scope will be executed if the
+ * condition is not met. If the condition is met, then the program execution
+ * will continue with the first instruction after the scope.
+ */
+
+struct bf_program;
+
+/**
+ * Cleanup attribute for a @ref bf_jmpctx variable.
+ */
+#define _cleanup_bf_jmpctx_ __attribute__((cleanup(bf_jmpctx_cleanup)))
+
+/**
+ * Create a new @ref bf_jmpctx variable.
+ *
+ * @param program The program to emit the jump instruction to. It must be
+ * non-NULL.
+ * @param insn The jump instruction to emit.
+ * @return A new @ref bf_jmpctx variable.
+ */
+#define bf_jmpctx_get(program, insn)                                           \
+    ({                                                                         \
+        size_t __idx = program->img_size;                                      \
+        int __r = bf_program_emit((program), (insn));                          \
+        if (__r < 0)                                                           \
+            return __r;                                                        \
+        (struct bf_jmpctx) {.program = program, .insn_idx = __idx};            \
+    })
+
+/**
+ * A helper structure to manage jump instructions in the program.
+ *
+ * @var bf_jmpctx::program
+ *  The program to emit the jump instruction to.
+ * @var bf_jmpctx::insn_idx
+ *  The index of the jump instruction in the program's image.
+ */
+struct bf_jmpctx
+{
+    struct bf_program *program;
+    size_t insn_idx;
+};
+
+/**
+ * Cleanup function for @ref bf_jmpctx.
+ *
+ * @param ctx The @ref bf_jmpctx variable to clean up.
+ */
+void bf_jmpctx_cleanup(struct bf_jmpctx *ctx);
