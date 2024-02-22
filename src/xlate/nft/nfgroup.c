b8d24a5709e6e5266467d67f35aef11add2fd8b2
Quentin Deslandes <qde@naccy.de>
2024-02-22 09:59:59 +0100

    xlate: nft: introduce bf_nfgroup
    This new structure will be used to group multiple Netlink messages
    together. This is useful when we want to send multiple messages back to
    nftables in a single response.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ src/xlate/nft/nfgroup.c
@@ -0,0 +1,105 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ */
+
+#include "xlate/nft/nfgroup.h"
+
+#include <linux/netfilter/nfnetlink.h>
+#include <linux/netlink.h>
+
+#include <errno.h>
+#include <limits.h>
+#include <netlink/msg.h>
+
+#include "core/list.h"
+#include "xlate/nft/nfmsg.h"
+
+struct bf_nfgroup
+{
+    bf_list messages;
+};
+
+int bf_nfgroup_new(struct bf_nfgroup **group)
+{
+    bf_assert(group);
+
+    _cleanup_bf_nfgroup_ struct bf_nfgroup *_group = NULL;
+
+    _group = calloc(1, sizeof(*_group));
+    if (!_group)
+        return -ENOMEM;
+
+    bf_list_init(&_group->messages, &(bf_list_ops) {
+                                        .free = (bf_list_ops_free)bf_nfmsg_free,
+                                    });
+
+    *group = TAKE_PTR(_group);
+
+    return 0;
+}
+
+int bf_nfgroup_new_from_stream(struct bf_nfgroup **group, struct nlmsghdr *nlh,
+                               size_t length)
+{
+    bf_assert(group);
+    bf_assert(nlh);
+    bf_assert(length <
+              INT_MAX); // nlmsg_ok() takes an int. length should not be larger
+                        // than INT_MAX, but we check anyway to be safe.
+
+    _cleanup_bf_nfgroup_ struct bf_nfgroup *_group = NULL;
+    int len = (int)length;
+    int r;
+
+    r = bf_nfgroup_new(&_group);
+    if (r < 0)
+        return r;
+
+    while (nlmsg_ok(nlh, len)) {
+        _cleanup_bf_nfmsg_ struct bf_nfmsg *msg = NULL;
+
+        if (nlh->nlmsg_type == NFNL_MSG_BATCH_BEGIN ||
+            nlh->nlmsg_type == NFNL_MSG_BATCH_END) {
+            // Skip batch messages.
+            nlh = nlmsg_next(nlh, &len);
+            continue;
+        }
+
+        r = bf_nfmsg_new_from_nlmsghdr(&msg, nlh);
+        if (r < 0)
+            return r;
+
+        r = bf_nfgroup_add_message(_group, msg);
+        if (r < 0)
+            return r;
+
+        TAKE_PTR(msg);
+
+        nlh = nlmsg_next(nlh, &len);
+    }
+
+    *group = TAKE_PTR(_group);
+
+    return 0;
+}
+
+void bf_nfgroup_free(struct bf_nfgroup **group)
+{
+    bf_assert(group);
+
+    if (!*group)
+        return;
+
+    bf_list_clean(&(*group)->messages);
+    free(*group);
+    *group = NULL;
+}
+
+int bf_nfgroup_add_message(struct bf_nfgroup *group, struct bf_nfmsg *msg)
+{
+    bf_assert(group);
+    bf_assert(msg);
+
+    return bf_list_add_tail(&group->messages, msg);
+}
--- tests/unit/harness/helper.h
+++ tests/unit/harness/helper.h
@@ -5,13 +5,17 @@
 
 #pragma once
 
+#include <stddef.h>
+
 #include "core/hook.h"
 
 #define _cleanup_tmp_file_ __attribute__((cleanup(bf_test_remove_tmp_file)))
 
 struct bf_codegen;
+struct nlmsghdr;
 
 char *bf_test_get_readable_tmp_filepath(void);
 void bf_test_remove_tmp_file(char **path);
 int bf_test_make_codegen(struct bf_codegen **codegen, enum bf_hook hook,
                          int nprogs);
+struct nlmsghdr *bf_test_get_nlmsghdr(size_t nmsg, size_t *len);
