16f43f58bca761005dfbe3e5370d069e55080ab0
Quentin Deslandes <qde@naccy.de>
2024-02-28 06:18:10 +0100

    opts: add option to disable nftables front
    Introduce --no-nftables option to disable nftables support.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

---
+++ tests/unit/src/opts.c
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2023 Meta Platforms, Inc. and affiliates.
+ */
+
+#include "src/opts.c"
+
+#include "harness/cmocka.h"
+#include "harness/helper.h"
+#include "harness/mock.h"
+
+Test(opts, no_nftables)
+{
+    char *opt0[] = {"tests_unit", "--no-nftables"};
+
+    _opts.fronts = 0xffff;
+    assert_int_equal(0, bf_opts_init(ARRAY_SIZE(opt0), opt0));
+    assert(0 == (_opts.fronts & (1 << BF_FRONT_NFT)));
+}
