6cb526bcc9086ebe5ec7761571b59fa8444a690c
Quentin Deslandes <qde@naccy.de>
2024-03-04 17:15:25 +0100

    xlate: nft: add support for NEWCHAIN, and GETCHAIN
    This patch adds support for the NFT_MSG_NEWCHAIN and NFT_MSG_GETCHAIN
    messages.
    
    bpfilter limits nftables to 1 chain attached to the prerouting hook, so
    bpfilter will ensure the parameters are valid and create a codegen
    with the requested policy accordingly. This chain can be updated if
    the same request, but with a different policy is received.
    
    NFT_MSG_GETCHAIN will return this chain, if it exists, containing
    the right policy and the number of rules currently loaded.
    
    Signed-off-by: Quentin Deslandes <qde@naccy.de>

--- src/xlate/nft/nft.c
+++ src/xlate/nft/nft.c
@@ -3,9 +3,16 @@
  * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
  */
 
+#include <arpa/inet.h>
+
+#include <linux/netfilter.h>
 #include <linux/netfilter/nf_tables.h>
 
+#include "core/context.h"
+#include "core/hook.h"
 #include "core/logger.h"
+#include "core/verdict.h"
+#include "generator/codegen.h"
 #include "shared/request.h"
 #include "shared/response.h"
 #include "xlate/front.h"
@@ -15,6 +22,7 @@
 struct bf_marsh;
 
 static const char *_bf_table_name = "bpfilter";
+static const char *_bf_chain_name = "prerouting";
 
 static int _bf_nft_setup(void);
 static int _bf_nft_teardown(void);
@@ -125,6 +133,162 @@ static int _bf_nft_newtable_cb(const struct bf_nfmsg *req)
     return 0;
 }
 
+static int _bf_nft_newchain_cb(const struct bf_nfmsg *req)
+{
+    bf_assert(req);
+
+    _cleanup_bf_codegen_ struct bf_codegen *codegen = NULL;
+    bf_nfattr *chain_attrs[__NFTA_CHAIN_MAX] = {};
+    bf_nfattr *hook_attrs[__NFTA_HOOK_MAX] = {};
+    enum bf_verdict verdict;
+    int r;
+
+    r = bf_nfmsg_parse(req, chain_attrs, __NFTA_CHAIN_MAX, bf_nf_chain_policy);
+    if (r < 0)
+        return bf_err_code(r, "failed to parse NFT_MSG_NEWCHAIN attributes");
+
+    if (!chain_attrs[NFTA_CHAIN_TABLE] ||
+        !bf_streq(bf_nfattr_get_str(chain_attrs[NFTA_CHAIN_TABLE]),
+                  _bf_table_name))
+        return bf_err_code(-EINVAL, "invalid table name");
+
+    if (!chain_attrs[NFTA_CHAIN_NAME] ||
+        !bf_streq(bf_nfattr_get_str(chain_attrs[NFTA_CHAIN_NAME]),
+                  _bf_chain_name))
+        return bf_err_code(-EINVAL, "invalid table name");
+
+    if (!chain_attrs[NFTA_CHAIN_POLICY])
+        return bf_err_code(-EINVAL, "missing NFTA_CHAIN_POLICY attribute");
+
+    if (!chain_attrs[NFTA_CHAIN_HOOK])
+        return bf_err_code(-EINVAL, "missing NFTA_CHAIN_HOOK attribute");
+
+    r = bf_nfattr_parse(chain_attrs[NFTA_CHAIN_HOOK], hook_attrs,
+                        __NFTA_HOOK_MAX, bf_nf_hook_policy);
+    if (r < 0)
+        return bf_err_code(r, "failed to parse NFTA_CHAIN_HOOK attributes");
+
+    if (!hook_attrs[NFTA_HOOK_HOOKNUM] ||
+        NF_INET_PRE_ROUTING !=
+            bf_nfattr_get_u32(hook_attrs[NFTA_HOOK_HOOKNUM])) {
+        return bf_err_code(
+            -EINVAL, "missing or invalid hook (NF_INET_PRE_ROUTING required)");
+    }
+
+    switch (ntohl(bf_nfattr_get_u32(chain_attrs[NFTA_CHAIN_POLICY]))) {
+    case NF_ACCEPT:
+        verdict = BF_VERDICT_ACCEPT;
+        break;
+    case NF_DROP:
+        verdict = BF_VERDICT_DROP;
+        break;
+    default:
+        return bf_err_code(
+            -ENOTSUP, "unsupported policy %u",
+            ntohl(bf_nfattr_get_u32(chain_attrs[NFTA_CHAIN_POLICY])));
+    };
+
+    codegen = bf_context_get_codegen(BF_HOOK_NFT_INGRESS, BF_FRONT_NFT);
+    if (codegen && verdict != codegen->policy) {
+        codegen->policy = verdict;
+        r = bf_codegen_update(codegen);
+        if (r < 0)
+            return bf_err_code(r, "failed to update codegen");
+
+        bf_info("existing codegen updated with new policy");
+    } else if (!codegen) {
+        r = bf_codegen_new(&codegen);
+        if (r < 0)
+            return bf_err_code(r, "failed to create bf_codegen");
+
+        codegen->front = BF_FRONT_NFT;
+        codegen->hook = BF_HOOK_NFT_INGRESS;
+        codegen->policy = verdict;
+
+        r = bf_codegen_generate(codegen);
+        if (r < 0)
+            return bf_err_code(r, "failed to generate codegen");
+
+        r = bf_codegen_load(codegen, NULL);
+        if (r < 0)
+            return bf_err_code(r, "failed to load codegen");
+
+        bf_context_set_codegen(BF_HOOK_NFT_INGRESS, BF_FRONT_NFT, codegen);
+
+        bf_info("new codegen created and loaded");
+    } else {
+        bf_info("codegen already properly configured, skipping generation");
+    }
+
+    TAKE_PTR(codegen);
+
+    return 0;
+}
+
+static int _bf_nft_getchain_cb(const struct bf_nfmsg *req,
+                               struct bf_nfgroup *res)
+{
+    bf_assert(req);
+    bf_assert(res);
+
+    struct bf_nfmsg *msg;
+    struct bf_codegen *codegen;
+    uint32_t policy;
+    int r;
+
+    // Only BF_HOOK_NFT_INGRESS is supported.
+    codegen = bf_context_get_codegen(BF_HOOK_NFT_INGRESS, BF_FRONT_NFT);
+    if (!codegen) {
+        /* If no codegen is found, do not fill the messages group and return
+         * success. The response message will then contain only a DONE
+         * message. */
+        return 0;
+    }
+
+    r = bf_nfgroup_add_new_message(res, &msg, NFT_MSG_NEWCHAIN,
+                                   bf_nfmsg_seqnr(req));
+    if (r < 0)
+        return bf_err_code(r, "failed to create bf_nfmsg");
+
+    bf_codegen_dump(codegen, NULL);
+
+    switch (codegen->policy) {
+    case BF_VERDICT_ACCEPT:
+        policy = NF_ACCEPT;
+        break;
+    case BF_VERDICT_DROP:
+        policy = NF_DROP;
+        break;
+    default:
+        return bf_err_code(-ENOTSUP, "unsupported codegen policy %u",
+                           codegen->policy);
+    };
+
+    bf_nfmsg_push_str_or_jmp(msg, NFTA_CHAIN_TABLE, _bf_table_name);
+    bf_nfmsg_push_str_or_jmp(msg, NFTA_CHAIN_NAME, _bf_chain_name);
+    bf_nfmsg_push_u64_or_jmp(msg, NFTA_CHAIN_HANDLE, BF_HOOK_NFT_INGRESS);
+    bf_nfmsg_push_u64_or_jmp(msg, NFTA_CHAIN_HANDLE, BF_HOOK_NFT_INGRESS);
+    bf_nfmsg_push_u32_or_jmp(msg, NFTA_CHAIN_POLICY, htonl(policy));
+    bf_nfmsg_push_str_or_jmp(msg, NFTA_CHAIN_TYPE, "filter");
+    bf_nfmsg_push_u32_or_jmp(msg, NFTA_CHAIN_FLAGS, NFT_CHAIN_BASE);
+    bf_nfmsg_push_u32_or_jmp(msg, NFTA_CHAIN_USE,
+                             htonl(bf_list_size(&codegen->rules)));
+
+    {
+        _cleanup_bf_nfnest_ struct bf_nfnest _ =
+            bf_nfnest_or_jmp(msg, NFTA_CHAIN_HOOK);
+
+        bf_nfmsg_push_u32_or_jmp(msg, NFTA_HOOK_HOOKNUM,
+                                 htonl(NF_INET_PRE_ROUTING));
+        bf_nfmsg_push_u32_or_jmp(msg, NFTA_HOOK_PRIORITY, htonl(0));
+    }
+
+    return 0;
+
+bf_nfmsg_push_failure:
+    return bf_err_code(-EINVAL, "failed to add attribute to Netlink message");
+}
+
 static int _bf_nft_request_handle(const struct bf_nfmsg *req,
                                   struct bf_nfgroup *res)
 {
@@ -142,6 +306,16 @@ static int _bf_nft_request_handle(const struct bf_nfmsg *req,
         break;
     case NFT_MSG_NEWTABLE:
         r = _bf_nft_newtable_cb(req);
+        break;
+    case NFT_MSG_GETCHAIN:
+        r = _bf_nft_getchain_cb(req, res);
+        break;
+    case NFT_MSG_NEWCHAIN:
+        r = _bf_nft_newchain_cb(req);
+        break;
+    case NFT_MSG_NEWRULE:
+        break;
+    case NFT_MSG_GETRULE:
         break;
     case NFT_MSG_GETOBJ:
     case NFT_MSG_GETFLOWTABLE:
