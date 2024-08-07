4af71ff2bd35e4bc2285d6c054290e25f93b3414
Quentin Deslandes <qde@naccy.de>
2024-08-07 17:53:50 +0200

    generator: matcher: use bf_matcher to create the BPF bytecode
    Generate a rule's bytecode using bf_rule.matchers (bf_matcher objects)
    instead of the matching criteria defined in the rule.
    
    The "old" matching criteria remain in the bf_rule structure and are
    still set during front-end translation, but they will be deleted in a
    subsequent commit.

---
+++ src/generator/matcher/ip.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
+ */
+
+#pragma once
+
+struct bf_matcher;
+struct bf_program;
+
+/**
+ * @brief Generate the bytecode for the BF_MATCHER_IP_* matcher types.
+ *
+ * @param program Program to generate the bytecode into. Can't be NULL.
+ * @param matcher Matcher to generate the bytecode for. Can't be NULL.
+ * @return 0 on success, negative errno value on failure.
+ */
+int bf_matcher_generate_ip(struct bf_program *program,
+                           const struct bf_matcher *matcher);
