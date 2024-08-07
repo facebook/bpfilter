eacab79b04dd4af7288aa2130089f80055384e0e
Quentin Deslandes <qde@naccy.de>
2024-08-07 09:23:49 +0200

    generator: printer: do not restore the map FD if it doesn't exist
    bf_printer won't create a map to store the debug strings if there are no
    debug strings used. Hence, on the next start of bpfilter, it shouldn't
    try to open the map pin file.

--- src/generator/printer.c
+++ src/generator/printer.c
@@ -226,9 +226,11 @@ int bf_printer_new_from_marsh(struct bf_printer **printer,
         TAKE_PTR(msg);
     }
 
-    r = bf_bpf_obj_get(_bf_printer_pin_path, &_printer->fd);
-    if (r < 0)
-        return bf_err_code(r, "failed to get printer map fd");
+    if (!bf_list_is_empty(&_printer->msgs)) {
+        r = bf_bpf_obj_get(_bf_printer_pin_path, &_printer->fd);
+        if (r < 0)
+            return bf_err_code(r, "failed to get printer map fd");
+    }
 
     *printer = TAKE_PTR(_printer);
 
