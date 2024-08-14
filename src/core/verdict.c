08a735afd1f2b521d3e1138ac9a8b1c26a1e8541
Quentin Deslandes <qde@naccy.de>
2024-08-14 12:32:50 +0200

    core: verdict: add function to convert a string to a verdict
    Introduce bf_verdict_type_from_str() to convert the string
    representation of a verdict into a verdict value.

--- src/core/verdict.c
+++ src/core/verdict.c
@@ -7,15 +7,32 @@
 
 #include "bpfilter/shared/helper.h"
 
+static const char *_bf_verdict_strs[] = {
+    [BF_VERDICT_ACCEPT] = "ACCEPT",
+    [BF_VERDICT_DROP] = "DROP",
+};
+
+static_assert(ARRAY_SIZE(_bf_verdict_strs) == _BF_VERDICT_MAX,
+              "missing entries in the verdict array");
+
 const char *bf_verdict_to_str(enum bf_verdict verdict)
 {
-    static const char *str[] = {
-        [BF_VERDICT_ACCEPT] = "ACCEPT",
-        [BF_VERDICT_DROP] = "DROP",
-    };
-
     bf_assert(0 <= verdict && verdict < _BF_VERDICT_MAX);
-    static_assert(ARRAY_SIZE(str) == _BF_VERDICT_MAX);
 
-    return str[verdict];
+    return _bf_verdict_strs[verdict];
+}
+
+int bf_verdict_from_str(const char *str, enum bf_verdict *verdict)
+{
+    bf_assert(str);
+    bf_assert(verdict);
+
+    for (size_t i = 0; i < _BF_VERDICT_MAX; ++i) {
+        if (bf_streq(_bf_verdict_strs[i], str)) {
+            *verdict = i;
+            return 0;
+        }
+    }
+
+    return -EINVAL;
 }
