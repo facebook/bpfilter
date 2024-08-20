8d41355e0e2634cede1b5ccb75f03d19a23e53e7
Quentin Deslandes <qde@naccy.de>
2024-08-20 16:28:29 +0200

    xlate: cli: implement support for cli front-end
    Add support for create a new chain or updating an existing chain in cli
    front-end.

--- src/xlate/cli.c
+++ src/xlate/cli.c
@@ -3,7 +3,11 @@
  * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
  */
 
+#include "core/chain.h"
+#include "core/context.h"
+#include "core/dump.h"
 #include "core/marsh.h"
+#include "generator/codegen.h"
 #include "shared/request.h"
 #include "shared/response.h"
 #include "xlate/front.h"
@@ -33,18 +37,87 @@ static int _bf_cli_teardown(void)
     return 0;
 }
 
+int _bf_cli_set_rules(const struct bf_request *request,
+                      struct bf_response **response)
+{
+    _cleanup_bf_chain_ struct bf_chain *chain = NULL;
+    struct bf_codegen *codegen;
+    int r;
+
+    bf_assert(request);
+    bf_assert(response);
+
+    r = bf_chain_new_from_marsh(&chain, (void *)request->data);
+    if (r)
+        return bf_err_code(r, "failed to create chain from marsh");
+
+    codegen = bf_context_get_codegen(chain->hook, BF_FRONT_CLI);
+    if (!codegen) {
+        r = bf_codegen_new(&codegen);
+        if (r)
+            return r;
+
+        codegen->hook = chain->hook;
+        codegen->front = BF_FRONT_CLI;
+    }
+
+    codegen->policy = chain->policy;
+    codegen->rules = chain->rules;
+    chain->rules.len = 0;
+    chain->rules.head = NULL;
+    chain->rules.tail = NULL;
+
+    if (bf_context_get_codegen(chain->hook, BF_FRONT_CLI)) {
+        r = bf_codegen_update(codegen);
+        if (r)
+            return bf_err_code(r, "failed to update codegen");
+    } else {
+        r = bf_codegen_up(codegen);
+        if (r)
+            return bf_err_code(r, "failed to load codegen");
+
+        r = bf_context_set_codegen(chain->hook, BF_FRONT_CLI, codegen);
+        if (r)
+            return bf_err_code(r, "failed to set codegen in context");
+    }
+
+    return bf_response_new_success(response, NULL, 0);
+}
+
 static int _bf_cli_request_handler(struct bf_request *request,
                                    struct bf_response **response)
 {
-    return 0;
+    int r;
+
+    bf_assert(request);
+    bf_assert(response);
+
+    if (request->data_len < sizeof(struct bf_marsh))
+        return bf_response_new_failure(response, -EINVAL);
+
+    switch (request->cmd) {
+    case BF_REQ_SET_RULES:
+        r = _bf_cli_set_rules(request, response);
+        break;
+    default:
+        r = bf_err_code(-EINVAL, "unsupported command %d for CLI front-end",
+                        request->cmd);
+        break;
+    }
+
+    return r;
 }
 
 static int _bf_cli_marsh(struct bf_marsh **marsh)
 {
+    UNUSED(marsh);
+
     return 0;
 }
 
 static int _bf_cli_unmarsh(struct bf_marsh *marsh)
 {
+    UNUSED(marsh);
+
     return 0;
 }
