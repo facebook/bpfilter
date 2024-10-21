9176278730fa62746fd5353791ce2e1fc53c787f
Quentin Deslandes <qde@naccy.de>
2024-10-21 15:50:27 +0200

    cgen: stub: read l4_proto as u8, not u16
    l4_proto is stored as an 8-bit value, reading it as a 16-bit value mess
    up the program verification (as it should). Fix it.

--- src/bpfilter/cgen/stub.c
+++ src/bpfilter/cgen/stub.c
@@ -253,7 +253,7 @@ int bf_stub_parse_l4_hdr(struct bf_program *program)
     /* Parse the L4 protocol and handle unuspported protocol, similarly to
      * bf_stub_parse_l3_hdr() above. */
     EMIT(program,
-         BPF_LDX_MEM(BPF_H, BF_ARG_4, BF_REG_CTX, BF_PROG_CTX_OFF(l4_proto)));
+         BPF_LDX_MEM(BPF_B, BF_ARG_4, BF_REG_CTX, BF_PROG_CTX_OFF(l4_proto)));
     {
         _cleanup_bf_swich_ struct bf_swich swich =
             bf_swich_get(program, BF_ARG_4);
