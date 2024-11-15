31e22070698b298b3b0bcfe42ddc50c8d355e3b4
Quentin Deslandes <qde@naccy.de>
2024-11-15 13:13:16 +0100

    core: set: add set type to match against the packets source (IPv6, port)
    This is a very specific set type expected to be used internally at Meta.
    Eventually, generic sets will allow for any set key to be dynamically
    generated.

--- src/core/set.c
+++ src/core/set.c
@@ -18,6 +18,7 @@ size_t _bf_set_type_elem_size(enum bf_set_type type)
 {
     static const size_t sizes[_BF_SET_MAX] = {
         [BF_SET_IP4] = 4,
+        [BF_SET_SRCIP6PORT] = 18,
     };
 
     static_assert(ARRAY_SIZE(sizes) == _BF_SET_MAX,
@@ -172,6 +173,7 @@ int bf_set_add_elem(struct bf_set *set, void *elem)
 
 static const char *_bf_set_type_strs[] = {
     [BF_SET_IP4] = "BF_SET_IP4",
+    [BF_SET_SRCIP6PORT] = "BF_SET_SRCIP6PORT",
 };
 
 static_assert(ARRAY_SIZE(_bf_set_type_strs) == _BF_SET_MAX, "");
