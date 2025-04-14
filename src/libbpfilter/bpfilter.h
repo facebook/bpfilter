977a77a637f216aa8a39334dbc3d87c59739505a
Quentin Deslandes <qde@naccy.de>
2025-04-14 07:42:02 +0200

    cli,lib,daemon: add chain {set,get,load,attach,update,flush}
    Add a set of library calls and bfcli commands to:
    - set: define a chain, discarding any chain with the same name
    - get: print a chain
    - load: generate and load a chain
    - attach: attach a loaded chain
    - update: update a loaded chain atomically
    - flush: delete a chain

--- src/libbpfilter/bpfilter.h
+++ src/libbpfilter/bpfilter.h
@@ -17,6 +17,7 @@ struct ipt_get_entries;
 struct ipt_replace;
 struct xt_counters_info;
 struct nlmsghdr;
+struct bf_hookopts;
 
 /**
  * Return the version of the library.
@@ -61,6 +62,119 @@ int bf_cli_ruleset_get(bf_list *chains, bf_list *hookopts, bf_list *counters);
 int bf_cli_ruleset_set(bf_list *chains, bf_list *hookopts);
 
 #undef bf_list
+
+/**
+ * Set a chain.
+ *
+ * If a chain with the same name already exist, it is detached and unloaded.
+ * The new chain is loaded, and attached if hook options are defined.
+ *
+ * The serialized data is formatted as:
+ * - Main marsh
+ *   - Chain marsh: contains `bf_chain` fields.
+ *   - Hook marsh: contains `bf_hookopts` fields, or empty is the chain is
+ *     not attached
+ *
+ * @param chain Chain to set. Can't be NULL.
+ * @param hookopts Hook options to attach the chain. If NULL, the chain is not
+ *        attached.
+ * @return 0 on success, or a negative errno value on failure.
+ */
+int bf_chain_set(struct bf_chain *chain, struct bf_hookopts *hookopts);
+
+/**
+ * Get a chain.
+ *
+ * If a chain with the same name already exist, `-EEXIST` is returned.
+ *
+ * The serialized data is formatted as:
+ * - Main marsh
+ *   - Marsh for the chain's name (including `\0`).
+ *
+ * Expects the following data:
+ * - Main mash
+ *   - Chain marsh: container `bf_chain` fields.
+ *   - Hook options marsh: contains `bf_hookopts` fields, or empty is the chain
+ *     is not attached.
+ *   - List marsh: contains marshes for the counters
+ *     - Counter marsh: contains `bf_counter` fields.
+ *     - ...
+ *
+ * @param name Name of the chain to look for. Can't be NULL.
+ * @param chain On success, contains a pointer to the chain. The caller is
+ *        responsible for freeing it. Can't be NULL.
+ * @param hookopts On success, contains a pointer to the chain's hook options if
+ *        the chain is attached, NULL otherwise. The caller is responsible for
+ *        freeing it. Can't be NULL.
+ * @param counters On success, the list contain the counters for every rule of
+ *        the chain, and the policy and error counters. The caller is
+ *        responsible for freeing it. Can't be NULL.
+ * @return 0 on success, or a negative errno value on failure, including:
+ * - `-ENOENT`: no chain found for this name.
+ */
+int bf_chain_get(const char *name, struct bf_chain **chain,
+                 struct bf_hookopts **hookopts, bf_list *counters);
+
+/**
+ * Load a chain.
+ *
+ * If a chain with the same name already exist, `-EEXIST` is returned.
+ *
+ * The serialized data is formatted as:
+ * - Main marsh
+ *   - Chain marsh: contains `bf_chain` fields.
+ *
+ * @param chain Chain to load. Can't be NULL.
+ * @return 0 on success, or a negative errno value on failure.
+ */
+int bf_chain_load(struct bf_chain *chain);
+
+/**
+ * Attach a chain.
+ *
+ * If the chain doesn't exist, `-ENOENT` is returned.
+ *
+ * The serialized data is formatted as:
+ * - Main marsh
+ *   - Marsh for the chain's name (including `\0`).
+ *   - Hook options marsh: contains `bf_hookopts` fields.
+ *
+ * @param name Name of the chain to attach. Can't be NULL.
+ * @param hookopts Hook options to attach the chain. Can't be NULL.
+ * @return 0 on success, or a negative errno value on failure, including:
+ * - `-ENOENT`: no chain found for this name.
+ * - `-EBUSY`: chain is already attached.
+ */
+int bf_chain_attach(const char *name, const struct bf_hookopts *hookopts);
+
+/**
+ * Update an attached chain.
+ *
+ * The chain to update must exist and be attached to a hook.
+ *
+ * The serialized data is formatted as:
+ * - Main marsh
+ *   - Chain marsh: container `bf_chain` fields.
+ *
+ * @param chain Chain to update. Can't be NULL.
+ * @return 0 on success, or a negative errno value on failure, including:
+ * - `-ENOENT`: no chain found for this name.
+ * - `-ENOLINK`: the chain to update is not attached.
+ */
+int bf_chain_update(const struct bf_chain *chain);
+
+/**
+ * Flush a chain (detach and unload).
+ *
+ * The serialized data is formatted as:
+ * - Main marsh
+ *   - Marsh for the chain's name (including `\0`).
+ *
+ * @param name Name of the chain to flush. Can't be NULL.
+ * @return 0 on success, or a negative errno value on failure, including:
+ * - `-ENOENT`: no chain found for this name.
+ */
+int bf_chain_flush(const char *name);
 
 /**
  * Send iptable's ipt_replace data to bpfilter daemon.
--- src/libbpfilter/cli.c
+++ src/libbpfilter/cli.c
@@ -217,3 +217,277 @@ int bf_cli_ruleset_set(bf_list *chains, bf_list *hookopts)
 
     return response->type == BF_RES_FAILURE ? response->error : 0;
 }
+
+int bf_chain_set(struct bf_chain *chain, struct bf_hookopts *hookopts)
+{
+    _cleanup_bf_request_ struct bf_request *request = NULL;
+    _cleanup_bf_response_ struct bf_response *response = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *marsh = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *chain_marsh = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *hook_marsh = NULL;
+    int r;
+
+    r = bf_marsh_new(&marsh, NULL, 0);
+    if (r)
+        return r;
+
+    r = bf_chain_marsh(chain, &chain_marsh);
+    if (r)
+        return r;
+
+    r = bf_marsh_add_child_obj(&marsh, chain_marsh);
+    if (r)
+        return r;
+
+    if (hookopts) {
+        r = bf_hookopts_marsh(hookopts, &hook_marsh);
+        if (r)
+            return r;
+
+        r = bf_marsh_add_child_obj(&marsh, hook_marsh);
+        if (r)
+            return r;
+    } else {
+        r = bf_marsh_add_child_raw(&marsh, NULL, 0);
+        if (r)
+            return r;
+    }
+
+    r = bf_request_new(&request, marsh, bf_marsh_size(marsh));
+    if (r)
+        return bf_err_r(r, "bf_chain_set: failed to create request");
+
+    request->front = BF_FRONT_CLI;
+    request->cmd = BF_REQ_CHAIN_SET;
+
+    r = bf_send(request, &response);
+    if (r)
+        return bf_err_r(r, "bf_chain_set: failed to send request");
+
+    return response->type == BF_RES_FAILURE ? response->error : 0;
+}
+
+int bf_chain_get(const char *name, struct bf_chain **chain,
+                 struct bf_hookopts **hookopts, bf_list *counters)
+{
+    _cleanup_bf_request_ struct bf_request *request = NULL;
+    _cleanup_bf_response_ struct bf_response *response = NULL;
+    _cleanup_bf_chain_ struct bf_chain *_chain = NULL;
+    _free_bf_hookopts_ struct bf_hookopts *_hookopts = NULL;
+    _clean_bf_list_ bf_list _counters = bf_list_default_from(*counters);
+    struct bf_marsh *marsh, *child = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *req_marsh = NULL;
+    int r;
+
+    r = bf_marsh_new(&req_marsh, NULL, 0);
+    if (r)
+        return r;
+
+    r = bf_marsh_add_child_raw(&req_marsh, name, strlen(name) + 1);
+    if (r)
+        return r;
+
+    r = bf_request_new(&request, req_marsh, bf_marsh_size(req_marsh));
+    if (r < 0)
+        return bf_err_r(r, "failed to init request");
+
+    request->front = BF_FRONT_CLI;
+    request->cmd = BF_REQ_CHAIN_GET;
+
+    r = bf_send(request, &response);
+    if (r < 0)
+        return bf_err_r(r, "failed to send a ruleset get request");
+
+    if (response->type == BF_RES_FAILURE)
+        return response->error;
+
+    marsh = (struct bf_marsh *)response->data;
+    if (bf_marsh_size(marsh) != response->data_len) {
+        return bf_err_r(
+            -EINVAL,
+            "response payload is expected to have the same size as the marsh");
+    }
+
+    if (!(child = bf_marsh_next_child(marsh, child)))
+        return -EINVAL;
+    r = bf_chain_new_from_marsh(&_chain, child);
+    if (r)
+        return r;
+
+    if (!(child = bf_marsh_next_child(marsh, child)))
+        return -EINVAL;
+    if (child->data_len) {
+        r = bf_hookopts_new_from_marsh(&_hookopts, child);
+        if (r)
+            return r;
+    }
+
+    if (!(child = bf_marsh_next_child(marsh, child)))
+        return -EINVAL;
+    for (struct bf_marsh *counter_marsh = bf_marsh_next_child(child, NULL);
+         counter_marsh;
+         counter_marsh = bf_marsh_next_child(child, counter_marsh)) {
+        _cleanup_bf_counter_ struct bf_counter *counter = NULL;
+
+        r = bf_counter_new_from_marsh(&counter, counter_marsh);
+        if (r)
+            return r;
+
+        r = bf_list_add_tail(&_counters, counter);
+        if (r)
+            return r;
+
+        TAKE_PTR(counter);
+    }
+
+    *chain = TAKE_PTR(_chain);
+    *hookopts = TAKE_PTR(_hookopts);
+    *counters = bf_list_move(_counters);
+
+    return 0;
+}
+
+int bf_chain_load(struct bf_chain *chain)
+{
+    _cleanup_bf_request_ struct bf_request *request = NULL;
+    _cleanup_bf_response_ struct bf_response *response = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *marsh = NULL;
+    int r;
+
+    r = bf_marsh_new(&marsh, NULL, 0);
+    if (r)
+        return r;
+
+    {
+        _cleanup_bf_marsh_ struct bf_marsh *child = NULL;
+
+        r = bf_chain_marsh(chain, &child);
+        if (r)
+            return r;
+
+        r = bf_marsh_add_child_obj(&marsh, child);
+        if (r)
+            return r;
+    }
+
+    r = bf_request_new(&request, marsh, bf_marsh_size(marsh));
+    if (r)
+        return bf_err_r(r, "bf_chain_load: failed to create a new request");
+
+    request->front = BF_FRONT_CLI;
+    request->cmd = BF_REQ_CHAIN_LOAD;
+
+    r = bf_send(request, &response);
+    if (r)
+        return bf_err_r(r, "bf_chain_set: failed to send request");
+
+    return response->type == BF_RES_FAILURE ? response->error : 0;
+}
+
+int bf_chain_attach(const char *name, const struct bf_hookopts *hookopts)
+{
+    _cleanup_bf_request_ struct bf_request *request = NULL;
+    _cleanup_bf_response_ struct bf_response *response = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *marsh = NULL;
+    int r;
+
+    r = bf_marsh_new(&marsh, NULL, 0);
+    if (r)
+        return r;
+
+    r = bf_marsh_add_child_raw(&marsh, name, strlen(name) + 1);
+    if (r)
+        return r;
+
+    {
+        _cleanup_bf_marsh_ struct bf_marsh *child = NULL;
+
+        r = bf_hookopts_marsh(hookopts, &child);
+        if (r)
+            return r;
+
+        r = bf_marsh_add_child_obj(&marsh, child);
+        if (r)
+            return r;
+    }
+
+    r = bf_request_new(&request, marsh, bf_marsh_size(marsh));
+    if (r)
+        return bf_err_r(r, "bf_chain_attach: failed to create a new request");
+
+    request->front = BF_FRONT_CLI;
+    request->cmd = BF_REQ_CHAIN_ATTACH;
+
+    r = bf_send(request, &response);
+    if (r)
+        return bf_err_r(r, "bf_chain_attach: failed to send request");
+
+    return response->type == BF_RES_FAILURE ? response->error : 0;
+}
+
+int bf_chain_update(const struct bf_chain *chain)
+{
+    _cleanup_bf_request_ struct bf_request *request = NULL;
+    _cleanup_bf_response_ struct bf_response *response = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *marsh = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *child = NULL;
+    int r;
+
+    r = bf_marsh_new(&marsh, NULL, 0);
+    if (r)
+        return r;
+
+    r = bf_chain_marsh(chain, &child);
+    if (r)
+        return r;
+
+    r = bf_marsh_add_child_obj(&marsh, child);
+    if (r)
+        return r;
+
+    r = bf_request_new(&request, marsh, bf_marsh_size(marsh));
+    if (r)
+        return bf_err_r(r, "bf_chain_update: failed to create a new request");
+
+    request->front = BF_FRONT_CLI;
+    request->cmd = BF_REQ_CHAIN_UPDATE;
+
+    r = bf_send(request, &response);
+    if (r)
+        return bf_err_r(r, "bf_chain_update: failed to send request");
+
+    return response->type == BF_RES_FAILURE ? response->error : 0;
+}
+
+int bf_chain_flush(const char *name)
+{
+    _cleanup_bf_request_ struct bf_request *request = NULL;
+    _cleanup_bf_response_ struct bf_response *response = NULL;
+    _cleanup_bf_marsh_ struct bf_marsh *marsh = NULL;
+    int r;
+
+    r = bf_marsh_new(&marsh, NULL, 0);
+    if (r)
+        return r;
+
+    r = bf_marsh_add_child_raw(&marsh, name, strlen(name) + 1);
+    if (r)
+        return r;
+
+    r = bf_marsh_add_child_raw(&marsh, name, strlen(name) + 1);
+    if (r)
+        return r;
+
+    r = bf_request_new(&request, marsh, bf_marsh_size(marsh));
+    if (r)
+        return bf_err_r(r, "failed to create request for chain");
+
+    request->front = BF_FRONT_CLI;
+    request->cmd = BF_REQ_CHAIN_FLUSH;
+
+    r = bf_send(request, &response);
+    if (r)
+        return bf_err_r(r, "failed to send chain to the daemon");
+
+    return response->type == BF_RES_FAILURE ? response->error : 0;
+}
