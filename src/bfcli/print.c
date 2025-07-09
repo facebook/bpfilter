40c8ec41a2144b82717b0c1bf9ade7d05e71df45
Quentin Deslandes <qde@naccy.de>
2025-07-09 11:41:42 +0200

    cli: do not print counters for rules without counters
    bfcli will print a 'counters ...' line for every rule when printing a
    chain or the ruleset, even if the rule doesn't have counters enabled.
    
    Check if the rule has counters enabled before printing it.

--- src/bfcli/print.c
+++ src/bfcli/print.c
@@ -134,9 +134,11 @@ void bfc_chain_dump(struct bf_chain *chain, struct bf_hookopts *hookopts,
             (void)fprintf(stdout, "\n");
         }
 
-        counter = bf_list_node_get_data(counter_node);
-        (void)fprintf(stdout, "        counters %lu packets %lu bytes\n",
-                      counter->packets, counter->bytes);
+        if (rule->counters) {
+            counter = bf_list_node_get_data(counter_node);
+            (void)fprintf(stdout, "        counters %lu packets %lu bytes\n",
+                          counter->packets, counter->bytes);
+        }
         counter_node = bf_list_node_next(counter_node);
 
         (void)fprintf(stdout, "        %s\n", bf_verdict_to_str(rule->verdict));
