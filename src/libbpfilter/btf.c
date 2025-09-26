1d01e2ea0e906eb61535e733721b7b5276551354
Quentin Deslandes <qde@naccy.de>
2025-09-26 05:44:50 +0200

    lib: btf: add support for nested struct/union field offset
    `bf_btf_get_field_off()` is used to find the offset of a field in a
    structure or union, based on the kernel BTF data. Only the direct fields
    are searched for, and nested anonymous structures and union are ignored,
    leading to the function failing for `sk_buff.mark`.
    
    This commit modifies `bf_btf_get_field_off()` to recursively search in
    the nested anonymous structures and unions of a given base structure.

--- src/libbpfilter/btf.c
+++ src/libbpfilter/btf.c
@@ -98,44 +98,116 @@ int bf_btf_kernel_has_token(void)
     return -ENOENT;
 }
 
-int bf_btf_get_field_off(const char *struct_name, const char *field_name)
+#define _bf_btf_type_is_compound(type)                                         \
+    (BTF_INFO_KIND((type)->info) == BTF_KIND_UNION ||                          \
+     BTF_INFO_KIND((type)->info) == BTF_KIND_STRUCT)
+
+/**
+ * @brief Find the offset of `field_name` in BTF type ID `compound_id`.
+ *
+ * @param compound_id Compound BTF type id.
+ * @param field_name Name of the field to find the offset of. Can't be NULL.
+ * @return Offset (in bits) of `field_name` in `compound_id` BTF type.
+ */
+static ssize_t _bf_btf_offset_in_compound(uint32_t compound_id,
+                                          const char *field_name)
 {
-    int offset = -1;
-    int struct_id;
-    struct btf_member *member;
-    const struct btf_type *type;
+    ssize_t offset;
+    const struct btf_type *compound_type, *member_type;
+    const struct btf_member *member;
 
-    struct_id = btf__find_by_name_kind(_bf_btf, struct_name, BTF_KIND_STRUCT);
-    if (struct_id < 0) {
-        return bf_err_r(struct_id, "can't find structure '%s' in kernel BTF",
-                        struct_name);
-    }
+    bf_assert(field_name);
 
-    type = btf__type_by_id(_bf_btf, struct_id);
-    if (!type)
-        return bf_err_r(errno, "can't get btf_type for '%s'", struct_name);
+    compound_type = btf__type_by_id(_bf_btf, compound_id);
+    if (!compound_type)
+        return -errno;
 
-    member = (struct btf_member *)(type + 1);
-    for (size_t i = 0; i < BTF_INFO_VLEN(type->info); ++i, ++member) {
-        const char *cur_name = btf__name_by_offset(_bf_btf, member->name_off);
-        if (!cur_name || !bf_streq(cur_name, field_name))
-            continue;
+    if (!_bf_btf_type_is_compound(compound_type))
+        return -ENOTSUP;
 
-        if (BTF_INFO_KFLAG(type->info)) {
-            offset = BTF_MEMBER_BIT_OFFSET(member->offset);
-        } else {
-            if (member->offset > INT_MAX) {
-                return bf_err_r(-E2BIG, "BTF member offset is too big: %u",
-                                member->offset);
+    member = (const struct btf_member *)(compound_type + 1);
+    for (size_t i = 0; i < BTF_INFO_VLEN(compound_type->info); ++i, ++member) {
+        const char *name = btf__name_by_offset(_bf_btf, member->name_off);
+
+        member_type = btf__type_by_id(_bf_btf, member->type);
+        if (!member_type)
+            return -errno;
+
+        // Member is an anonymous compound type? Check its members
+        if (*name == '\0' && _bf_btf_type_is_compound(member_type)) {
+            offset = _bf_btf_offset_in_compound(member->type, field_name);
+            if (offset < 0)
+                continue;
+
+            if (BTF_MEMBER_BIT_OFFSET(member->offset) % 8) {
+                // Assuming this is not possible, but who knows
+                return bf_err_r(-ENOTSUP,
+                                "anonymous compound parent is a bitfield");
             }
-            offset = (int)member->offset;
+
+            if (BTF_INFO_KFLAG(compound_type->info))
+                offset += BTF_MEMBER_BIT_OFFSET(member->offset);
+            else
+                offset += member->offset;
+
+            return offset;
         }
 
-        break;
+        if (!bf_streq(name, field_name))
+            continue;
+
+        offset = (ssize_t)member->offset;
+        if (BTF_INFO_KFLAG(compound_type->info))
+            offset = BTF_MEMBER_BIT_OFFSET(member->offset);
+
+        return offset;
     }
 
-    if (offset < 0 || offset % 8)
-        return -ENOENT;
+    return -EINVAL;
+}
 
-    return offset / 8;
+static int _bf_btf_get_compound_type_id(const char *name)
+{
+    int compound_id;
+
+    compound_id = btf__find_by_name_kind(_bf_btf, name, BTF_KIND_STRUCT);
+    if (compound_id < 0 && compound_id != -ENOENT)
+        return compound_id;
+    if (compound_id >= 0)
+        return compound_id;
+
+    compound_id = btf__find_by_name_kind(_bf_btf, name, BTF_KIND_UNION);
+    if (compound_id < 0 && compound_id != -ENOENT)
+        return compound_id;
+
+    return compound_id;
+}
+
+int bf_btf_get_field_off(const char *struct_name, const char *field_name)
+{
+    int id;
+    ssize_t offset;
+
+    bf_assert(struct_name);
+    bf_assert(field_name);
+
+    id = _bf_btf_get_compound_type_id(struct_name);
+    if (id < 0)
+        return bf_err_r(id, "failed to find BTF type ID for '%s'", struct_name);
+
+    offset = _bf_btf_offset_in_compound(id, field_name);
+    if (offset < 0) {
+        return bf_err_r((int)offset, "failed to find offset of %s.%s",
+                        struct_name, field_name);
+    }
+    if (offset % 8) {
+        return bf_err_r(-EINVAL, "%s.%s has a bit offset", struct_name,
+                        field_name);
+    }
+    if (offset / 8 > INT_MAX) {
+        return bf_err_r(-E2BIG, "%s.%s has an offset bigger than %d",
+                        struct_name, field_name, INT_MAX);
+    }
+
+    return (int)(offset / 8);
 }
