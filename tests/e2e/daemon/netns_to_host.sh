01b8ca34e29f6e55c1a1ebaffaa72c393eb2b1f7
Quentin Deslandes <qde@naccy.de>
2025-11-24 10:45:54 +0000

    tests: e2e: remove early exit from netns_to_sh

--- tests/e2e/daemon/netns_to_host.sh
+++ tests/e2e/daemon/netns_to_host.sh
@@ -12,5 +12,5 @@ start_bpfilter
 ${FROM_NS} ping -c 1 -W 0.1 ${HOST_IP_ADDR}
 ${FROM_NS} bfcli ruleset set --from-str "chain xdp BF_HOOK_TC_INGRESS{ifindex=${NS_IFINDEX}} ACCEPT rule ip4.proto icmp log link,internet counter DROP"
 (! ping -c 1 -W 0.1 ${NS_IP_ADDR})
-${FROM_NS} bfcli chain get --name xdp | awk '/log link,internet/{getline; print $2}' | grep -q "^1$" && exit 0 || exit 1
+${FROM_NS} bfcli chain get --name xdp | awk '/log link,internet/{getline; print $2}' | grep -q "^1$"
 ${FROM_NS} bfcli ruleset flush
