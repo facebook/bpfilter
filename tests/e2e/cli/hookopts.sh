171ffdf00fde8802269dc934146db8d6d818573a
Quentin Deslandes <qde@naccy.de>
2026-01-28 12:38:53 +0100

    cli: error-out on duplicated hook options
    When parsing hook options, bpfilter doesn't check if a given options is
    already set and override it instead. This leads to memory leaks for
    cgpath= option as the string is duplicated.
    
    Prevent users from providing duplicated options by check if the option
    is set before parsing the value.

---
+++ tests/e2e/cli/hookopts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
+
+. "$(dirname "$0")"/../e2e_test_util.sh
+
+# Disallow duplicated hook options
+(! bfcli ruleset set --dry-run --from-str "chain ifindex BF_HOOK_XDP{ifindex=2,ifindex=3} ACCEPT")
+(! bfcli ruleset set --dry-run --from-str "chain cgpath BF_HOOK_CGROUP_INGRESS{cgpath=/sys/fs/cgroup,cgpath=/sys/fs/cgroup} ACCEPT")
+(! bfcli ruleset set --dry-run --from-str "chain family BF_HOOK_NF_LOCAL_IN{family=inet4,family=inet6} ACCEPT")
+(! bfcli ruleset set --dry-run --from-str "chain priorities BF_HOOK_NF_LOCAL_IN{priorities=1-2,priorities=3-4} ACCEPT")
