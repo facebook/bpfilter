846eca61bae9b0059dcceabeaf8c81de792a6d17
Quentin Deslandes <qde@naccy.de>
2026-01-31 12:08:38 +0100

    tests: e2e: remove dependecy on bpftool for pin_updated_chain.sh

--- tests/e2e/daemon/pin_updated_chain.sh
+++ tests/e2e/daemon/pin_updated_chain.sh
@@ -7,19 +7,21 @@ set -o pipefail
 
 make_sandbox
 
+PINNED_PROG="${WORKDIR}/bpf/bpfilter/test_chain/bf_prog"
+
 start_bpfilter
     ${FROM_NS} bfcli chain set --from-str "chain test_chain BF_HOOK_XDP{ifindex=${NS_IFINDEX}} ACCEPT"
     ${FROM_NS} ping -c 1 -W 0.1 ${NS_IP_ADDR}
     ${FROM_NS} bfcli ruleset get | grep "^chain" | awk 'END{exit NR!=1}'
-    bpftool prog | grep "name bf_prog" | awk 'END{exit NR!=1}'
+    ${FROM_NS} test -e ${PINNED_PROG}
 
     ${FROM_NS} bfcli chain update --from-str "chain test_chain BF_HOOK_XDP{ifindex=${NS_IFINDEX}} ACCEPT rule meta.l4_proto eq icmp DROP"
     (! ping -c 1 -W 0.1 ${NS_IP_ADDR})
     ${FROM_NS} bfcli ruleset get | grep "^chain" | awk 'END{exit NR!=1}'
-    bpftool prog | grep "name bf_prog" | awk 'END{exit NR!=1}'
+    ${FROM_NS} test -e ${PINNED_PROG}
 stop_bpfilter --skip-cleanup
 
 start_bpfilter
     ${FROM_NS} bfcli ruleset get | grep "^chain" | awk 'END{exit NR!=1}'
-    bpftool prog | grep "name bf_prog" | awk 'END{exit NR!=1}'
+    ${FROM_NS} test -e ${PINNED_PROG}
 stop_bpfilter
