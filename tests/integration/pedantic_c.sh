d7f631d04a4c9378e6d727725e5bae5157ea8cae
Quentin Deslandes <qde@naccy.de>
2026-01-31 12:06:43 +0100

    tests: e2e: pass library path as env to pedantic tests

--- tests/integration/pedantic_c.sh
+++ tests/integration/pedantic_c.sh
@@ -3,20 +3,24 @@
 set -eux
 set -o pipefail
 
-_ROOT_DIR=${ROOT_DIR:?}
 _BUILD_DIR=${BUILD_DIR:?}
 _GEN_INC_DIR=${GEN_INC_DIR:?}
-LIB_INC_DIR=${_ROOT_DIR}/src/libbpfilter/include
+_LIB_INC_DIR=${LIB_INC_DIR:?}
 
-HEADERS=$(find ${LIB_INC_DIR} -type f -name "*.h" -exec realpath --relative-to ${LIB_INC_DIR} {} \;)
+# If GEN_INC_DIR points to a file, use its parent directory
+if [ -f "${_GEN_INC_DIR}" ]; then
+    _GEN_INC_DIR=$(dirname $(dirname "${_GEN_INC_DIR}"))
+fi
 
-# Start with version.h as it's not in the source folder
+HEADERS=$(find ${_LIB_INC_DIR} -type f -name "*.h" -exec realpath --relative-to ${_LIB_INC_DIR} {} \;)
+
+# Start with version.h as it's not in the source folder
 INCLUDES="#include <bpfilter/version.h>"
 
-for header in $(find ${LIB_INC_DIR} -type f -name "*.h" -exec realpath --relative-to ${LIB_INC_DIR} {} \; | sort); do
+for header in $(find ${_LIB_INC_DIR} -type f -name "*.h" -exec realpath --relative-to ${_LIB_INC_DIR} {} \; | sort); do
     INCLUDES="${INCLUDES}\n#include <${header}>"
 done
 
 SRC="${INCLUDES}\nint main(void) { return 0; }"
 
-echo -e ${SRC} | gcc -x c -I"${LIB_INC_DIR}" -I"${_GEN_INC_DIR}" -pedantic-errors -std=c17 -Werror -Wall -Wextra -c -o /dev/null -
+echo -e ${SRC} | gcc -x c -I"${_LIB_INC_DIR}" -I"${_GEN_INC_DIR}" -pedantic-errors -std=c17 -Werror -Wall -Wextra -c -o /dev/null -
