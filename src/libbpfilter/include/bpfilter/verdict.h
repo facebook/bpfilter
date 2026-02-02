1b9ccf856fd55bbd954b32c4066cc76f5e4ea959
Quentin Deslandes <qde@naccy.de>
2026-02-02 10:10:54 +0100

    daemon,lib: add REDIRECT verdict
    Introduce a new REDIRECT rule verdict to pass a packet to another
    interface when a rule matches.
    
    This new verdict is only supported with TC and XDP (egress only): when a
    rule with REDIRECT is matched, the packet is redirect to the specific
    interface on ingress or egress.

--- src/libbpfilter/include/bpfilter/verdict.h
+++ src/libbpfilter/include/bpfilter/verdict.h
@@ -6,6 +6,35 @@
 #pragma once
 
 /**
+ * Redirect direction for the REDIRECT verdict.
+ */
+enum bf_redirect_dir
+{
+    /** Redirect to the interface's ingress path. */
+    BF_REDIRECT_INGRESS,
+    /** Redirect to the interface's egress path. */
+    BF_REDIRECT_EGRESS,
+    _BF_REDIRECT_DIR_MAX,
+};
+
+/**
+ * @brief Convert a redirect direction value into a string.
+ *
+ * @param dir The direction to convert, must be valid.
+ * @return String representation of the direction.
+ */
+const char *bf_redirect_dir_to_str(enum bf_redirect_dir dir);
+
+/**
+ * @brief Convert a string into a redirect direction value.
+ *
+ * @param str String to convert to a direction. Can't be NULL.
+ * @param dir Pointer to store the direction value. Can't be NULL.
+ * @return 0 on success, or negative errno value on error.
+ */
+int bf_redirect_dir_from_str(const char *str, enum bf_redirect_dir *dir);
+
+/**
  * Verdict to apply for a rule or chain.
  * Chains can only use terminal verdicts, rules can use all verdicts.
  */
@@ -19,8 +48,10 @@ enum bf_verdict
     /** Non-terminal verdicts that allow further packet processing. */
     /** Continue processing the next rule. */
     BF_VERDICT_CONTINUE,
+    /** Redirect the packet to another interface. */
+    BF_VERDICT_REDIRECT,
     _BF_VERDICT_MAX,
-    _BF_TERMINAL_VERDICT_MAX = BF_VERDICT_CONTINUE,
+    _BF_TERMINAL_VERDICT_MAX = BF_VERDICT_REDIRECT,
 };
 
 /**
