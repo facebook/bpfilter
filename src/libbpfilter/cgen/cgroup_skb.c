60cd790af424e6177af5c29699d6277518cea8a1
yaakov-stein <ymstein14@gmail.com>
2026-03-20 10:43:20 -0700

    lib: cgen: move packet codegen out of matcher directory
    Move packet.c/h from cgen/matcher/ to cgen/ and rename
    bf_matcher_generate_packet() to bf_packet_gen_inline_matcher(). This
    file provides shared codegen utilities for all packet-based flavors,
    not a matcher implementation, and will host additional shared packet
    codegen like bf_packet_gen_inline_log() in subsequent commits.

--- src/libbpfilter/cgen/cgroup_skb.c
+++ src/libbpfilter/cgen/cgroup_skb.c
@@ -19,7 +19,7 @@
 #include <bpfilter/verdict.h>
 
 #include "cgen/matcher/cmp.h"
-#include "cgen/matcher/packet.h"
+#include "cgen/packet.h"
 #include "cgen/program.h"
 #include "cgen/stub.h"
 #include "cgen/swich.h"
@@ -132,7 +132,7 @@ static int _bf_cgroup_skb_gen_inline_matcher(struct bf_program *program,
         return bf_cmp_value(program, bf_matcher_get_op(matcher),
                             bf_matcher_payload(matcher), 4, BPF_REG_1);
     default:
-        return bf_matcher_generate_packet(program, matcher);
+        return bf_packet_gen_inline_matcher(program, matcher);
     }
 }
 
